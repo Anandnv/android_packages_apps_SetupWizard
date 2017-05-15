bd81f8a8e36613716ee2e8214b6bc4f1083af350
Anandnv <anand.nv1998@gmail.com>
2017-05-15 15:41:45 +0530

    AIM INITIAL SETUP ohhh FUCK YEAHH

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := MoKeeSetupWizard
+LOCAL_PACKAGE_NAME := AIMSetupWizard
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Provision
@@ -16,8 +16,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
     libphonenumber \
-    org.mokee.platform.internal \
-    org.mokee.platform.sdk
+    org.aim.platform.internal \
+    org.aim.platform.sdk
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common
